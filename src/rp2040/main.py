3f05af803a67efa993d5a169540bbc0f018e692a
Thadeus Reed <t@creativecontrol.cc>
2024-05-13 16:40:34 -0700

    Update main.py

--- src/rp2040/main.py
+++ src/rp2040/main.py
@@ -11,16 +11,36 @@ import control_loop
 from note_map import *
 from config import *
 import dacs
+import serial_cdc
 DEBUG = False
 
 if DEBUG:
     print("fluxharmonium")
 
+dac_values = [
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+            [255, 255, 255, 255, 255, 255, 255, 255],
+        ]
+
 # Prepare the DACs (super rough)
-dacs = dacs.DACs(CONTROL_RATE, envelope_values, velocity_values)
+dacs = dacs.DACs(CONTROL_RATE, dac_values)
+
+# Prepare the serial
+serial = serial_cdc.SerialCDC(CONTROL_RATE, dac_values)
 
 cl = control_loop.ControlLoop(CONTROL_RATE)
 
 cl.add_control(dacs.run())
+cl.add_control(serial.run())
 
 cl.run()
