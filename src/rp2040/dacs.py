bffcfb09bb89d76cc47fdc40c8790bd7858a149a
Thadeus Reed <t@creativecontrol.cc>
2024-05-13 16:39:56 -0700

    Update dacs.py

--- src/rp2040/dacs.py
+++ src/rp2040/dacs.py
@@ -8,50 +8,18 @@ import creativecontrol_circuitpython_ltc166x
 
 class DACs():
     """
-    Setup a DAC control loop to update and output the dac values by combining
-    the envelope and velocity values
+    Setup a DAC control loop to output the dac values
     """
-    def __init__(self, frequency,debug=False):
+    def __init__(self, frequency, dac_values, debug=False):
         self.frequency = frequency
         self.debug = debug
-        self.dac_values = [
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-            [255, 255, 255, 255, 255, 255, 255, 255],
-        ]
+        self.dac_values = dac_values
 
         self.ltc1665 = creativecontrol_circuitpython_ltc166x.LTC1665(
             csel=board.GP1, sck=board.GP2, mosi=board.GP3, debug=False
         )
 
         self.ltc1665.write_chained_dac_values(self.dac_values)
-    
-    def get_group_index(self, position):
-        """
-        Retrieving the matrix 2D position
-        """
-        output = position
-        if self.debug:
-            print("original note: ", output)
-
-        group = int((output / 8))
-        index = int((output % 8))
-
-        if self.debug:
-            print("modified note: ", output)
-            print("group: ", group)
-            print("index: ", index)
-
-        return {"group": group, "index": index}
     
     def freq_to_sec(self, freq):
         return 1000/1000/freq
